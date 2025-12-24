81d547d9e0eaf9a9f1c9b53457fb5e986fd957d8
Klimm-Max <klimm.max@gmail.com>
2025-12-24 11:12:55 +0100

    file format

--- src/main/kotlin/api/JsonTemplateEngine.kt
+++ src/main/kotlin/api/JsonTemplateEngine.kt
@@ -2,8 +2,6 @@ package io.github.klimmmax.api
 
 import io.github.klimmmax.core.TemplateRenderer
 
-class JsonTemplateEngine internal constructor(
-    private val renderer: TemplateRenderer
-) {
+class JsonTemplateEngine internal constructor(private val renderer: TemplateRenderer) {
     fun render(template: String): String = renderer.render(template)
 }
