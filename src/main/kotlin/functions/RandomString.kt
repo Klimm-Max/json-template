9749783517786c830f175f2fffb71fd8cd1a72a8
Klimm-Max <klimm.max@gmail.com>
2025-12-24 11:13:12 +0100

    adding RandomString() template function and tests

---
+++ src/main/kotlin/functions/RandomString.kt
@@ -0,0 +1,51 @@
+package io.github.klimmmax.functions
+
+import io.github.klimmmax.api.TemplateFunction
+import io.github.klimmmax.core.ExecutionContext
+
+internal class RandomString : TemplateFunction {
+    override val name = "randomString"
+
+    companion object {
+        private const val MAX_LENGTH = 255
+    }
+
+    override fun execute(args: List<String>, ctx: ExecutionContext): String {
+        return when (args.size) {
+            0 -> getRandomString(32, ctx)
+
+            1 -> {
+                val max = args[0].toIntOrNull()
+                    ?: throw IllegalArgumentException("randomString(max): max must be an integer, got '${args[0]}'")
+                require(max in 0..MAX_LENGTH) {
+                    throw IllegalArgumentException("randomString(max): max length must be  between 0 and 255, got '${args[0]}'")
+                }
+
+                getRandomString(max, ctx)
+            }
+
+            2 -> {
+                val max = args[0].toIntOrNull()
+                    ?: throw IllegalArgumentException("randomString(max, prefix): max must be an integer, got '${args[0]}'")
+                require(max in 0..MAX_LENGTH) {
+                    throw IllegalArgumentException("randomString(max, prefix): max length must be  between 0 and 255, got '${args[0]}'")
+                }
+
+                args[1] + getRandomString(max, ctx)
+            }
+
+            else -> throw IllegalArgumentException(
+                "randomString expects 0, 1 or 2 arguments but got ${args.size}"
+            )
+        }
+    }
+
+    private fun getRandomString(length: Int, ctx: ExecutionContext): String {
+        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+        return buildString(length) {
+            repeat(length) {
+                append(allowedChars.random(ctx.random))
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/functions/RandomStringTest.kt
@@ -0,0 +1,65 @@
+package functions
+
+import io.github.klimmmax.core.ExecutionContext
+import io.github.klimmmax.functions.RandomString
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import kotlin.random.Random
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+class RandomStringTest {
+
+    private lateinit var ctx: ExecutionContext
+    private lateinit var fn: RandomString
+
+    @BeforeEach
+    fun setup() {
+        ctx = ExecutionContext(random = Random(123))
+        fn = RandomString()
+    }
+
+    @Test
+    fun `no args returns any string with length of 32`() {
+        val result = fn.execute(emptyList(), ctx)
+        // deterministic because of seed
+        assertEquals(32, result.length)
+        assertEquals("ON6DJSIc6Wms5O4NgmKbUVWXG5ck2LbH", result)
+    }
+
+    @Test
+    fun `one arg is used as max length`() {
+        val result = fn.execute(listOf("10"), ctx)
+        assertEquals(result.length, 10)
+        assertEquals("ON6DJSIc6W", result)
+    }
+
+    @Test
+    fun `two args use max length and substring`() {
+        val result = fn.execute(listOf("10", "prefixString-"), ctx)
+        assertEquals(result.length, 23)
+        assertEquals("prefixString-ON6DJSIc6W", result)
+    }
+
+    @Test
+    fun `non integer arg or invalid value as max length throws`() {
+        assertFailsWith<IllegalArgumentException> {
+            fn.execute(listOf("abc"), ctx)
+        }
+
+        assertFailsWith<IllegalArgumentException> {
+            fn.execute(listOf("256"), ctx)
+        }
+
+        assertFailsWith<IllegalArgumentException> {
+            fn.execute(listOf("-12", "prefixString"), ctx)
+        }
+    }
+
+    @Test
+    fun `too many args throws`() {
+        assertFailsWith<IllegalArgumentException> {
+            fn.execute(listOf("1", "2", "3"), ctx)
+        }
+    }
+}
